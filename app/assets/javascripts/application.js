070cb8bc4516d95e419aa01235ecbbfb9ba8e738
Mark Kaylor <mkaylor.guitar@gmail.com>
2018-05-07 18:37:32 +0200

    added js and scss import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
-//= require materialize
-//= require materialize-sprockets
