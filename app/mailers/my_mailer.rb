ca55a29209f1d32bd58d21cd9ed438cdbfe75217
Mark Kaylor <mkaylor.guitar@gmail.com>
2018-05-10 23:55:38 +0200

    remove sensitive info

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,6 +1,6 @@
 class MyMailer < ApplicationMailer
   def sendgrid_client
-    @sendgrid_client ||= SendGrid::API.new(api_key: "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")
+    @sendgrid_client ||= SendGrid::API.new(api_key: "")
   end
 
   def new_user(user)
@@ -22,7 +22,7 @@ class MyMailer < ApplicationMailer
         }
       ],
       "from": {
-        "email": "mkaylor.guitar@gmail.com"
+        "email": ""
       },
       "template_id": "028d37ca-0b24-4aef-951e-ca2579defda2"
     }
