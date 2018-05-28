e37363313ff2f5193cacdad2a2b16687d5eb2929
Mark Kaylor <mkaylor.guitar@gmail.com>
2018-05-28 16:26:09 -0400

    remove drop down from mobile navbar

--- app/models/course.rb
+++ app/models/course.rb
@@ -12,7 +12,7 @@ class Course < ApplicationRecord
   has_many :users, through: :subscriptions
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :content, presence: true, length: { maximum: 500 }
+  validates :content, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
 
   has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
